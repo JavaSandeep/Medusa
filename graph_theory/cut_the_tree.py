34e8ce2c25691b4f46eaf9f3fe062232bd4829e8
Sandeep Dudhraj <dudhraj.sandeep@gmail.com>
2020-07-14 13:09:54 +0800

    Changes in cut_the_Tree.py: post record

--- graph_theory/cut_the_tree.py
+++ graph_theory/cut_the_tree.py
@@ -1,12 +1,13 @@
 #!/bin/python3
 
+#!/bin/python3
+
 import math
 import os
 import random
 import re
 import sys
 
-from utils import measure_time
 #
 # Complete the 'cutTheTree' function below.
 #
@@ -15,28 +16,34 @@ from utils import measure_time
 #  1. INTEGER_ARRAY data
 #  2. 2D_INTEGER_ARRAY edges
 #
-from collections import deque
 
-visited=set()
 cum_weight=dict()
-
-min_t=[float('inf')]
 
 child_parent=dict()
 
-def dfs_iterative(G, s, n):
-    total = sum(n)
+min_t=[float('inf')]
+
+def dfs_iterative(G, s, weights):
+    total = sum(weights)
+
     stack = [s]
+    visited = set()
 
     while stack:
         vertex = stack[-1]
         if vertex in visited:
-            # revisiting the node
-            cum_weight[vertex]=n[vertex-1]
+            # logic
+            # cum sum of a node: it's weight+ all it's children's weight
+            # logic
+            cum_weight[vertex]=weights[vertex-1]
             for c in G[vertex]:
                 if child_parent.get(c,-1)==vertex:
                     cum_weight[vertex]+=cum_weight[c]
-            min_t[0]=min(min_t[0],abs(total-2*cum_weight[vertex]))
+            # difference
+            min_t[0]=min(
+                min_t[0],
+                abs(total-2*cum_weight[vertex])
+            )
             stack.pop()
             continue
         visited.add(vertex)
@@ -44,17 +51,20 @@ def dfs_iterative(G, s, n):
             if neighbor not in visited:
                 child_parent[neighbor]=vertex
                 stack.append(neighbor)
+    
+    return visited
 
 def setGraph(nCount, edges):
     """
-    Fastest method to set graph
+    Boilerplate method to set graph
     returns graph
     :param: nCount number of nodes in graph - n nodes
     :param: edges list of edges in graph
     """
-    # setting the nodes
     g=dict()
 
+    # setting the nodes
+    # create dict with key and value as node
     for _t,_e in edges:
         if _t not in g.keys():
             g[_t]=[_e]
@@ -65,14 +75,16 @@ def setGraph(nCount, edges):
         else:
             g[_e].append(_t)
     
+    # updates the dict for zombie nodes
     g.update({k:[] for k in range(1,nCount+1) if k not in g.keys()})
     
     return g
 
+
 def cutTheTree(nodes, edges):
     # Write your code here
-    # weightMap.update({k+1:v for k,v in enumerate(nodes)})
     G=setGraph(len(nodes),edges)
+    print(G)
     dfs_iterative(G,1,nodes) #<- 1 will act as root node # with the maximum cumulative sum
     return min_t[0]
 
